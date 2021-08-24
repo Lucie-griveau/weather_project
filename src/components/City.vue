37875ff6a5b93e9bae0744d1413d038308da40cf
Lucie GRIVEAU <lucie.griveau@Digital-Grenoble.local>
2021-08-24 16:06:23 +0200

    Premier projet vue.js - 3.Création composant City

---
+++ src/components/City.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="city">
+    <h1>Bienvenue à {{ name }}</h1>
+    <h2>Aujourd'hui, la météo annonce: {{ weather }}</h2>
+    <p>La température au sol est actuellement de: {{ temperature }} °C</p>
+    <p>Date de dernière mise à jour: {{ updatedAt.toLocaleString() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "City",
+  props: {
+    msg: String
+  },
+  data (){
+    return {
+      name: 'Grenoble', // nom de la ville
+      weather: 'Peu nuageux', // descriptif météo
+      temperature: 20.55, // température en °C
+      updatedAt: new Date() // date de dernière mise à jour
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,16 @@
 <template>
   <div class="hello">
-    <h1>Bienvenue à {{ name }}</h1>
-    <h2>Aujourd'hui, la météo annonce: {{ weather }}</h2>
-    <p>La température au sol est actuellement de: {{ temperature }} °C</p>
-    <p>Date de dernière mise à jour: {{ updatedAt.toLocaleString() }}</p>
+    <City/>
   </div>
 </template>
 
 <script>
+import City from './City.vue'
+
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
-  },
-  data (){
-    return {
-      name: 'Grenoble', // nom de la ville
-      weather: 'Peu nuageux', // descriptif météo
-      temperature: 20.55, // température en °C
-      updatedAt: new Date() // date de dernière mise à jour
-    }
+  name: 'HelloWorld',
+  components: {
+    City
   }
 }
 </script>
